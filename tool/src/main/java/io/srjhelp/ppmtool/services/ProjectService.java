5e81285c07299d1ecee2c07d9d2a5ce6c1b35561
Saroj Pandey <sarojraj11@gmail.com>
2020-07-03 13:33:55 +0200

    finished l7

--- tool/src/main/java/io/srjhelp/ppmtool/services/ProjectService.java
+++ tool/src/main/java/io/srjhelp/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.srjhelp.ppmtool.services;
 
 import io.srjhelp.ppmtool.domain.Project;
+import io.srjhelp.ppmtool.exceptions.ProjectIdException;
 import io.srjhelp.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public  Project saveOrUpdateProject(Project project){
-    //Lot of logic
-        return  projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+
+        }
     }
 }
